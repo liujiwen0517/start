5c19d4adb600a4d77b61641dca06cdf1b214d059
liujiwen <liujiwen@51dojo.com>
2018-09-18 20:17:29 +0800

    del

--- email_creditScore_20180202/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ANACONDA_HOME=/root/anaconda2
-# ROOT_DIR=`realpath $(dirname $0)`
-ROOT_DIR=/email_creditScore/credit_v1.0
-# cd $ROOT_DIR/src && CUDA_VISIBLE_DEVICES="" $ANACONDA_HOME/bin/gunicorn -w 1 -D -b 0.0.0.0:6023 -p $ROOT_DIR/log/predicate_service.pid zsd_credit_model_flask:app
-cd /email_creditScore/credit_v1.0/src && CUDA_VISIBLE_DEVICES="" $ANACONDA_HOME/bin/gunicorn -w 2 -D -b 0.0.0.0:6033 -p $ROOT_DIR/log/predicate_service.pid scorecard_flask:app
